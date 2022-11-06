b328aa011193f40872013b26f935d583b914f93c
ksk-14 <ksk14.info@gmail.com>
2022-11-06 16:12:03 +0900

    React 設定関係整理

---
+++ resources/js/home.jsx
@@ -0,0 +1,8 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('home')
+);
---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/commonPart.css">
+    <title>Anisatsu</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/home.jsx'])
+</head>
+
+<body>
+    <header>
+        <img class="logoImg" src="img/logo.svg" alt="ヘッダーロゴ画像">
+        <nav class="nav">
+            <ul>
+                <li class="menuItem"><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="bodySpace">
+        <div id="home"></div>
+    </div>
+</body>
+
+</html>
